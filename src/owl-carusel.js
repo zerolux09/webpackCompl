2f1def2ee3c19b593b3ee956d24c744810555a61
zerolux09 <zerolux09@gmail.com>
2020-08-17 23:13:35 +0300

    owl

--- src/owl-carusel.js
+++ src/owl-carusel.js
@@ -2101,68 +2101,72 @@ break;
 // --------------------------------------------------
 // owl carousel setup
 // --------------------------------------------------
-    testiSlider()
+(function($) {
+  // USE STRICT
+  'use strict';
+  testiSlider();
 
-    function testiSlider() {
-        const aboutSlide = $('.testimonial .owl-carousel')
-        aboutSlide.owlCarousel({
-            loop: true,
-            margin: 30,
-            mouseDrag: true,
-            autoplay: true,
-            center: false,
-            dots: false,
-            dragEndSpeed: 700,
-            smartSpeed: 2000,
-            responsiveClass: true,
-            autoplayHoverPause: true,
-            autoplayTimeout: 2000,
-            responsive: {
-                0: {
-                    items: 1,
-                    margin: 0,
-                },
-                600: {
-                    items: 1,
-                    margin: 0,
-                },
-                1000: {
-                    items: 1,
-                    margin: 0,
-                }
-            }
-        })
-    }
+  function testiSlider() {
+    const aboutSlide = $('.testimonial .owl-carousel');
+    aboutSlide.owlCarousel({
+      loop: true,
+      margin: 30,
+      mouseDrag: true,
+      autoplay: true,
+      center: false,
+      dots: false,
+      dragEndSpeed: 700,
+      smartSpeed: 2000,
+      responsiveClass: true,
+      autoplayHoverPause: true,
+      autoplayTimeout: 2000,
+      responsive: {
+        0: {
+          items: 1,
+          margin: 0,
+        },
+        600: {
+          items: 1,
+          margin: 0,
+        },
+        1000: {
+          items: 1,
+          margin: 0,
+        },
+      },
+    });
+  }
 
-    singleSlider()
+  singleSlider();
 
-    function singleSlider() {
-        const singleSlide = $('.project-slide .owl-carousel')
-        singleSlide.owlCarousel({
-            loop: true,
-            margin: 30,
-            mouseDrag: true,
-            autoplay: true,
-            center: false,
-            dots: true,
-            dragEndSpeed: 700,
-            smartSpeed: 2000,
-            responsiveClass: true,
-            autoplayHoverPause: true,
-            autoplayTimeout: 2000,
-            responsive: {
-                0: {
-                    items: 1,
-                    margin: 0,
-                },
-                600: {
-                    items: 2,
-                    margin: 0,
-                },
-                1000: {
-                    items: 3,
-                    margin: 30,
-                }
-            }
-        })
-    }
+  function singleSlider() {
+    const singleSlide = $('.project-slide .owl-carousel');
+    singleSlide.owlCarousel({
+      loop: true,
+      margin: 30,
+      mouseDrag: true,
+      autoplay: true,
+      center: false,
+      dots: true,
+      dragEndSpeed: 700,
+      smartSpeed: 2000,
+      responsiveClass: true,
+      autoplayHoverPause: true,
+      autoplayTimeout: 2000,
+      responsive: {
+        0: {
+          items: 1,
+          margin: 0,
+        },
+        600: {
+          items: 2,
+          margin: 0,
+        },
+        1000: {
+          items: 3,
+          margin: 30,
+        },
+      },
+    });
+  }
+})(jQuery);
